2af64ad9466206c3ff8016887d11f54445814d3e
shakyab <bishal@window2theworld.org>
2024-03-12 13:12:17 +0000

    Update Node.js version and add Pinia to Nuxt modules

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
+<script setup>
+import { useCounterStore } from '~/store';
+
+const store = useCounterStore();
+console.log(store.name);
+</script>
 <template>
     <div>
         <p class="text-red-400 font-bold text-3xl">index</p>
